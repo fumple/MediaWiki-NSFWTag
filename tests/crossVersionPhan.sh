99f891b165e556eba239c1e6d3b66bab1e960cb5
Fumple <me@fumple.pl>
2022-08-02 18:57:10 +0200

    Update phan test script

--- tests/crossVersionPhan.sh
+++ tests/crossVersionPhan.sh
@@ -3,7 +3,9 @@
 # Removes used docker images afterwards
 VERSIONS="1.35.6 1.35.7 1.37.3 1.38.2"
 TBOLD="\e[1m"
+TRESULT="$TBOLD\e[35m"
 TINFO="$TBOLD\e[35m"
+TERR="$TBOLD\e[31m"
 TRESET="\e[0m"
 SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 cd $SCRIPTDIR/..
@@ -12,6 +14,10 @@ composer update
 for ver in $VERSIONS; do
     echo -e "$TINFO=== Pulling $ver ===$TRESET";
     docker image pull mediawiki:$ver
+    if [[ $? -gt 0 ]]; then
+        echo -e "$TERR=== Failed to pull $ver, skipping... ===$TRESET";
+        continue
+    fi
 
     echo -e "$TINFO=== Running phan for $ver ===$TRESET";
     docker run --rm \
@@ -19,8 +25,12 @@ for ver in $VERSIONS; do
     -v $PWD:/var/www/html/extensions/NSFWTag:ro \
     mediawiki:$ver \
     /var/www/html/extensions/NSFWTag/tests/dockerPrepAndRunPhan.sh
+    if [[ $? -gt 0 ]]; then
+        echo -e "$TERR=== Phan found issues! ===$TRESET";
+        continue
+    fi
 
     echo -e "$TINFO=== Cleaning up after $ver ===$TRESET";
-    docker image rm mediawiki:$ver
+    docker image rm mediawiki:$ver > /dev/null
 done
 echo -e "$TINFO=== Finished ===$TRESET";
